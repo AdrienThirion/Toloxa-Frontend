c1c7eb04171fa2a1c7db9210a961f13705dd0141
AdrienThirion <55891808+AdrienThirion@users.noreply.github.com>
2025-03-27 09:49:11 +0100

    Add information

--- backend/setup.py
+++ backend/setup.py
@@ -12,14 +12,14 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="YourProjectName",  # Replace with your project's name
+    name="Toloxa Website",  # Replace with your project's name
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A short description of your project",
+    author="Adrien Thirion",
+    author_email="adrien.thirion@toloxa.com",
+    description="Vocal assistant for auto-reparation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/yourproject",  # Replace with your repository URL
+    url="https://github.com/AdrienThirion/Toloxa-Frontend",  # Replace with your repository URL
     packages=find_packages(),  # Automatically find packages in your project
     install_requires=requirements,  # Install dependencies from requirements.txt
     python_requires=">=3.9, <3.13",  # Enforce Python version
