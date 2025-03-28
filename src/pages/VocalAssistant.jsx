9c13f3a1715ec780d30ee6cf423c4cf9e37c6c25
AdrienThirion <55891808+AdrienThirion@users.noreply.github.com>
2025-03-28 11:25:56 +0100

    Update VocalAssistant.jsx

--- src/pages/VocalAssistant.jsx
+++ src/pages/VocalAssistant.jsx
@@ -129,16 +129,17 @@ function VocalAssistant() {
       // 2) Create PeerConnection + handlers
     const createPeerConnection = () => {
         
-        const peerConnection = new RTCPeerConnection({
-            iceServers: [
-              { urls: "stun:global.stun.twilio.com:3478" },
-              {
-                urls: ["turn:global.turn.twilio.com:443?transport=tcp"],
-                username: "818f375d7989b13d6959ebf28e7b7e4a636c28fed331e734ec0c094e7012964d",
-                credential: "hFU2FnLar5AxDihYLJLaG/0uEW9f7nwFMeug7iX0yhM="
-              }
-            ]
-          });
+      const peerConnection = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "turn:global.turn.twilio.com:443?transport=tcp"
+            ],
+            username: "818f375d7989b13d6959ebf28e7b7e4a636c28fed331e734ec0c094e7012964d",
+            credential: "hFU2FnLar5AxDihYLJLaG/0uEW9f7nwFMeug7iX0yhM="
+          }
+        ]
+      });
           
 
         peerConnection.addTransceiver("audio", { direction: "recvonly" });
